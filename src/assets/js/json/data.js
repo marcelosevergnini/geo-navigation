8e2d8d198ae82e0975264ef25a144e977d53c415
Marcelo Severgnini <marcelosevergnini@gmail.com>
2018-01-17 12:01:05 +0000

    added new postions to data

--- src/assets/js/json/data.js
+++ src/assets/js/json/data.js
@@ -1,8 +1,7 @@
 var dataList = [
-    { id: 1, nameBranch: 'diconiun Lisbon', longitude: -9.141870, latitude: 38.717756},
-    { id: 2, nameBranch: 'diconium Stuttgart', longitude: 9.212767, latitude: 48.813548},
-    { id: 3, nameBranch: 'diconium São José', longitude: -122.057543, latitude: 37.387474},
-    { id: 4, nameBranch: 'diconium Karlsruhe', longitude: 8.403653, latitude: 49.006890},
-    { id: 5, nameBranch: 'diconium Hamburg', longitude: 9.993682, latitude: 53.551085},
-    { id: 6, nameBranch: 'diconium Berlin', longitude: 13.404954, latitude: 52.520007}
+    { id: 1, nameBranch: 'Nova Roma do Sul', longitude: -51.4099630, latitude: -28.9833710},
+    { id: 2, nameBranch: 'Chapecó', longitude: -52.6156990, latitude: -27.1009340},
+    { id: 3, nameBranch: 'Caxias do Sul', longitude: -51.1796680, latitude: -29.1634030},
+    { id: 4, nameBranch: 'Dublin', longitude: -6.2603100, latitude: 53.3498050},
+    { id: 5, nameBranch: 'Lisbon', longitude: 9.993682, latitude: 53.551085}
 ];
