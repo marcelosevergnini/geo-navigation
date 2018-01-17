2022d2ea3bf9200ef230a375bb863661e64a0ab9
Marcelo Severgnini <marcelosevergnini@gmail.com>
2018-01-17 12:06:00 +0000

    added new postions to data

--- src/assets/js/json/data.js
+++ src/assets/js/json/data.js
@@ -3,5 +3,9 @@ var dataList = [
     { id: 2, nameBranch: 'Chapecó', longitude: -52.6156990, latitude: -27.1009340},
     { id: 3, nameBranch: 'Caxias do Sul', longitude: -51.1796680, latitude: -29.1634030},
     { id: 4, nameBranch: 'Dublin', longitude: -6.2603100, latitude: 53.3498050},
-    { id: 5, nameBranch: 'Lisbon', longitude: 9.993682, latitude: 53.551085}
-];
+    { id: 5, nameBranch: 'Lisbon', longitude: 9.993682, latitude: 53.551085},
+    { id: 5, nameBranch: 'Milan', longitude: 9.1899820, latitude: 45.4642040},
+    { id: 5, nameBranch: 'Torino', longitude: 7.6868560, latitude: 45.0703120},
+    { id: 5, nameBranch: 'Porto', longitude: -8.6291050, latitude: 41.1579440},
+    { id: 5, nameBranch: 'Stuttgart', longitude: 9.1829320, latitude: 48.7758460},
+    { id: 5, nameBranch: 'Paris', longitude: 2.3522220, latitude: 48.8566140}
