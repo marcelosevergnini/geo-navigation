14decc39b8b51d4c86e1f946648f39154cdeaff2
Marcelo Severgnini <marcelosevergnini@gmail.com>
2018-01-22 16:51:14 +0000

    chnage wrong cordinate

--- src/assets/js/json/data.js
+++ src/assets/js/json/data.js
@@ -3,7 +3,7 @@ var dataList = [
     { id: 2, nameBranch: 'Chapecó', longitude: -52.6156990, latitude: -27.1009340},
     { id: 3, nameBranch: 'Caxias do Sul', longitude: -51.1796680, latitude: -29.1634030},
     { id: 4, nameBranch: 'Dublin', longitude: -6.2603100, latitude: 53.3498050},
-    { id: 5, nameBranch: 'Lisbon', longitude: 9.993682, latitude: 53.551085},
+    { id: 5, nameBranch: 'Lisbon', longitude: -9.142685, latitude: 38.736946},
     { id: 5, nameBranch: 'Milan', longitude: 9.1899820, latitude: 45.4642040},
     { id: 5, nameBranch: 'Torino', longitude: 7.6868560, latitude: 45.0703120},
     { id: 5, nameBranch: 'Porto', longitude: -8.6291050, latitude: 41.1579440},
