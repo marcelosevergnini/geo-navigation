7f89db4553582c0fbe1d2e2c054e3d64efdc6f22
Marcelo Severgnini <marcelosevergnini@gmail.com>
2018-01-22 16:53:10 +0000

    added two new util functions, createPin that load a pin from obj file and createSimpleSphere to create a sphere geometry to use in many cases

--- src/assets/js/util/util.js
+++ src/assets/js/util/util.js
@@ -17,9 +17,51 @@ UTIL.Functions.createMarker = function(){
     return marker;
 }
 
+UTIL.Functions.createPin = function(){
+    var marker = new THREE.Object3D();
+        var loader = new THREE.OBJLoader();
+        loader.load( 'src/assets/models/Pin.obj', function ( object ) {
+            object.traverse( function ( child ) {
+                if (child instanceof THREE.Mesh ) {
+                    child.material = new THREE.MeshPhongMaterial( { 
+                        color: 0xff5c01, 
+                        specular: 0x050505,
+                        shininess: 100
+                    });
+                    var geometry = new THREE.Geometry().fromBufferGeometry( child.geometry );
+                    geometry.computeFaceNormals();
+                    geometry.mergeVertices();
+                    geometry.computeVertexNormals();
+                    child.geometry = new THREE.BufferGeometry().fromGeometry( geometry );
+                    object.scale.set(0.005,0.005,0.005);
+                    object.position.set(51, 0, 0);
+                    object.quaternion.setFromUnitVectors(new THREE.Vector3(0, 1, 0), new THREE.Vector3(1, 0, 0));
+                }
+            });
+            object.rotateX((70 * Math.PI) / 180); 
+            object.rotateY((125 * Math.PI) / 180);
+            object.rotateZ((0 * Math.PI) / 180);
+            marker.add(object);
+        });
+        return marker;  
+}
+
 UTIL.Functions.setGuiObjectRotation = function(object){
     var gui = new dat.GUI();     
     gui.add( {z: 1}, 'z', -200, 200 ).step(5).onChange( function( value ){ object.fov = value;});
     gui.add( {x: 1}, 'x', -200, 200 ).step(6).onChange( function( value ){object.rotation.x = value;});
     gui.add( {y: 1}, 'y', -200, 200 ).step(7).onChange( function( value ){object.rotation.y = value;});
+}
+
+UTIL.Functions.createSimpleSphere = function(rad, widthSegments, heightSegments, posX, posY, posZ){
+        
+    var geometry	= new THREE.SphereGeometry(rad, widthSegments, heightSegments)
+    var material	= new THREE.MeshPhongMaterial({
+        bumpScale	: 0.05,
+    })
+    var object = new THREE.Object3D();
+    object= new THREE.Mesh(geometry, material)
+    object.position.set(posX, posY, posZ);
+
+    return object;
 }
