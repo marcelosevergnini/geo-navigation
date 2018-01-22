9a20648f99ac4d2b7c11010762bcf4f4a78ddf76
Marcelo Severgnini <marcelosevergnini@gmail.com>
2018-01-22 16:49:44 +0000

    changed some functions from js files, removed duplicated function

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -7,25 +7,22 @@ var demo = (function() {
     var CANVAS_WIDTH = window.innerWidth,CANVAS_HEIGHT = window.innerHeight;
     var objectGroup = new THREE.Object3D();
     var earthObject = {}
-    var tweens = {};
-    tweens.runners = [];
-    tweens.processId = 0;
-    
+    var objectToMove;
+
     var createGeometry = function(){
         earthObject.earthMesh = PLANET_UTIL.Planets.createEarth();
         earthObject.cloudsMesh = PLANET_UTIL.Planets.createEarthCloud();
         earthObject.earth = new THREE.Object3D();
+        var sphereAxis = new THREE.AxesHelper(100);
         earthObject.earth.add(earthObject.earthMesh);
         earthObject.earth.add(earthObject.cloudsMesh);
+        earthObject.earth.add(sphereAxis);
         objectGroup.add(earthObject.earth);
+        objectToMove = UTIL.Functions.createSimpleSphere(0.5, 32, 32, 0, 0, 130);
+        objectGroup.add(objectToMove);
         scene.add(objectGroup);
     },
-    convertLatLonToVec3	= function(lat,lon){
-        lat =  lat * Math.PI / 180.0;
-        lon = -lon * Math.PI / 180.0;
-        return new THREE.Vector3(Math.cos(lat) * Math.cos(lon), Math.sin(lat), Math.cos(lat) * Math.sin(lon));
-    },
-    creaSkyBox = function(){
+    createSkyBox = function(){
    
         var texture	= new THREE.TextureLoader().load('src/assets/textures/galaxy_starfield.png')
         var material = new THREE.MeshBasicMaterial({map	: texture, side : THREE.BackSide})
@@ -34,39 +31,15 @@ var demo = (function() {
         objectGroup.add(mesh);
 
     },
-    createPin = function(id){
-
-        var marker = new THREE.Object3D();
-        var loader = new THREE.OBJLoader();
-        loader.load( 'src/assets/models/Pin.obj', function ( object ) {
-            object.traverse( function ( child ) {
-                if (child instanceof THREE.Mesh ) {
-                    child.material = new THREE.MeshPhongMaterial( { 
-                        color: 0xff5c01, 
-                        specular: 0x050505,
-                        shininess: 100
-                    });
-                    object.scale.set(0.005,0.005,0.005);
-                    object.position.set(51, 0, 0);
-                    object.quaternion.setFromUnitVectors(new THREE.Vector3(0, 1, 0), new THREE.Vector3(1, 0, 0));
-                }
-            });
-            object.rotateX((70 * Math.PI) / 180);
-            object.rotateY((125 * Math.PI) / 180);
-            object.rotateZ((0 * Math.PI) / 180);
-            marker.add(object);
-        });
-        return marker;  
-    },
     setMarker = function(listBranchList){
+
         var rad = Math.PI / 180;
               
         listBranchList.forEach(function(element) {
-            var marker = createPin();    
+            var marker = UTIL.Functions.createPin();    
             marker.name = element.nameBranch.replace(/ /g, '-').toLowerCase();
             marker.quaternion.setFromEuler(new THREE.Euler(0, element.longitude * rad, element.latitude * rad, "YZX")); 
             markers.push(marker);
-            //cameraContainer.camera.add(marker);
             earthObject.earth.add(marker);
         });
 
@@ -78,7 +51,7 @@ var demo = (function() {
         container.appendChild(renderer.domElement);
     },
     setControls = function () {
-        controls = new THREE.OrbitControls( cameraContainer.camera);
+        controls = new THREE.TrackballControls(cameraContainer.camera);
     },
     lights = function () {
         var lightA = new THREE.DirectionalLight(0xffffff);
@@ -102,59 +75,32 @@ var demo = (function() {
         updateScene();
     },
     setCamera = function(){
+       
         cameraContainer.camera = new THREE.PerspectiveCamera(45, CANVAS_WIDTH / CANVAS_HEIGHT, 1, 100000);
         cameraContainer.camera.position.set(0, 0, 150);
         cameraContainer.cameraOriginalPosition = new THREE.Vector3(cameraContainer.camera.position.x, cameraContainer.camera.position.y, cameraContainer.camera.position.z);
-    },
-    createTweens = function(){
-
-        tweens.original = new TWEEN.Tween(cameraContainer.camera.position).to(cameraContainer.cameraOriginalPosition, 3000).easing(TWEEN.Easing.Quadratic.Out)
-        .onStart(function(){})
-        .onComplete(function(){
-            tweens.processId = tweens.processId + 1;
-            if(tweens.processId > dataList.length){
-                tweens.processId = 1;
-            }
-            tweens.runners[tweens.processId].delay(1000).start();
-        });
-
-        dataList.forEach(function(element) {
-            var currentPosition = UTIL.Functions.convertLatLonToVec3(element.latitude,element.longitude).multiplyScalar(52.5);
-            //var current = { x: cameraContainer.camera.position.x, y: cameraContainer.camera.position.y, z: cameraContainer.camera.position.z  };
-            //var target = { x: (currentPosition.x), y: (currentPosition.y), z: 0};
-            var currentTween = new TWEEN.Tween(cameraContainer.camera.position).to(currentPosition, 3000).easing(TWEEN.Easing.Quadratic.Out)
-            .onStart(function(){})
-            .onComplete(function(){});
-            currentTween.chain(tweens.original.delay(3000));
-            tweens.runners.push(currentTween);
-        });      
+      
     },
     init = function () {
 
         container = document.getElementById("container");
         loadRender();
-      
         lights();
         createGeometry();    
         setCamera();
-        creaSkyBox();
+        createSkyBox();
         setMarker(dataList);
         setControls();
         //UTIL.Functions.setGuiObjectRotation(cameraContainer.camera);
-        createTweens();
-        tweens.runners[tweens.processId].delay(3000).start();
+        TWEENS.runners.createTweensObjectByGeoPosition(dataList, objectToMove);
+        TWEENS.runners.runnersContainer[TWEENS.runners.processId].delay(3000).start();
         animate();
-    },
-    addZoom = function(value){
-        cameraContainer.camera.position.z = value;
     }
 
     window.onload = init;
     
     return {
-        scene: scene,
-        tweens : tweens,
-        addZoom : addZoom
+        scene: scene
     }
 
 })();
