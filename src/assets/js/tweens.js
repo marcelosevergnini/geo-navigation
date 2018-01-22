7667843aed87fb02c121ff0888a33ad4dfbc4abb
Marcelo Severgnini <marcelosevergnini@gmail.com>
2018-01-22 16:50:33 +0000

    created tweensjs to have the animation functions

---
+++ src/assets/js/tweens.js
@@ -0,0 +1,61 @@
+var TWEENS = TWEENS || {}
+
+TWEENS.runners	= {}
+TWEENS.runners.runnersContainer = [];
+TWEENS.runners.processId = 0;
+
+TWEENS.runners.createTweensByGeoPosition = function(dataList, cameraContainer, onStartExec, onCompleteExec){
+
+    TWEENS.runners.originalPosition = new TWEEN.Tween(cameraContainer.camera.position).to(cameraContainer.cameraOriginalPosition, 3000).easing(TWEEN.Easing.Quadratic.Out)
+    .onStart(function(){
+
+    })
+    .onComplete(function(){
+        TWEENS.runners.processId = TWEENS.runners.processId + 1;
+        if(TWEENS.runners.processId > dataList.length){
+            TWEENS.runners.processId = 1;
+        }
+        TWEENS.runners.runnersContainer[TWEENS.runners.processId].delay(1000).start();
+    });
+
+    dataList.forEach(function(element) {
+        var currentPosition = UTIL.Functions.convertLatLonToVec3(element.latitude,element.longitude).multiplyScalar(52.5);
+        //var current = { x: cameraContainer.camera.position.x, y: cameraContainer.camera.position.y, z: cameraContainer.camera.position.z  };
+        //var target = { x: (currentPosition.x), y: (currentPosition.y), z: 0};
+        var currentTween = new TWEEN.Tween(cameraContainer.camera.position).to(currentPosition, 3000).easing(TWEEN.Easing.Circular.Out)
+        .onStart(function(){
+
+        })
+        .onComplete(function(){
+
+        });
+        currentTween.chain(TWEENS.runners.originalPosition.delay(3000));
+        TWEENS.runners.runnersContainer.push(currentTween);
+    });  
+}
+
+TWEENS.runners.createTweensObjectByGeoPosition = function(dataList, objectToMove, onStartExec, onCompleteExec){
+
+    dataList.forEach(function(element) {
+
+        var targetPosition = UTIL.Functions.convertLatLonToVec3(element.latitude,element.longitude).multiplyScalar(52.5);
+        
+        var currentTween = new TWEEN.Tween(objectToMove.position).to(targetPosition, 3000).easing(TWEEN.Easing.Circular.Out)
+
+        .onStart(function(){
+            onCompleteExec != undefined ? onStartExec() : "";
+        })
+        .onComplete(function(){
+            onCompleteExec != undefined ? onCompleteExec() : "";
+
+            TWEENS.runners.processId = TWEENS.runners.processId + 1;
+            if(TWEENS.runners.processId > dataList.length){
+                TWEENS.runners.processId = 1;
+            }
+            TWEENS.runners.runnersContainer[TWEENS.runners.processId].delay(3000).start();
+            
+        });
+
+        TWEENS.runners.runnersContainer.push(currentTween);
+    });         
+}  
